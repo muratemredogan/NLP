037b2e58b2dcf1a63403a5251f1b06d8c09e8a52
MURAT-mr07 <150949780+MURAT-mr07@users.noreply.github.com>
2024-12-28 01:36:22 +0300

    main

---
+++ Olasılıksal_Dil_Modelleri/max_ent.py
@@ -0,0 +1,18 @@
+from nltk.classify import MaxentClassifier
+
+# egitim veri seti
+train_data = [
+    ({"love":True, "amazing":True}, "positive"), 
+    ({"hate":True, "terrible":True}, "negative"),
+    ({"happy":True, "joy":True}, "positive"),
+    ({"sad":True, "depressed":True}, "negative")]
+
+# max ent sınıflandırıcı
+classifier = MaxentClassifier.train(train_data, max_iter = 10)
+
+# test
+test_sentence = "I do not like this movie"
+features = {word: (word in test_sentence.lower().split()) for word in ["love", "amazing", "hate", "terrible","happy", "joy","sad", "depressed"]}
+
+label = classifier.classify(features)
+print(label)
