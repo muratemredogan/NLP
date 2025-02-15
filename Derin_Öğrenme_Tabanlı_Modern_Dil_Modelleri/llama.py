75cf89a409c26d61c62007639cfe891190960468
muratemredogan <220911763@stu.istinye.edu.tr>
2025-02-15 23:27:09 +0300

    main

--- Derin_Öğrenme_Tabanlı_Modern_Dil_Modelleri/llama.py
+++ Derin_Öğrenme_Tabanlı_Modern_Dil_Modelleri/llama.py
@@ -1,19 +1,19 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
-# modeli ve tokenizer yukle
+# model ve tokenizer yukle
 model_name = "huggyllama/llama-7b"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForCausalLM.from_pretrained(model_name)
 
-# ornek baslangic cumlesi
+# ornek baslangic metni
 text = "I go to swim for"
 
-# tokenize
-inputs = tokenizer.encode(text, return_tensors="pt")
+# token
+inputs = tokenizer(text, return_tensors = "pt")
 
 # metin tamamlama
-outputs = model.generate(inputs.input_ids, max_length=10)
+outputs = model.generate(inputs.input_ids, max_length = 10)
 
 # decode
-genereted_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
-print(genereted_text)
+generated_text = tokenizer.decode(outputs[0], skip_special_tokens = True)
+print(generated_text)
